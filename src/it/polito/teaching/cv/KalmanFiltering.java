114637d4fc81713bb3d8a9f1a67d52c45cd10e0d
edu <edun327@gmail.com>
2016-09-07 01:15:08 -0300

    Solo faltan 50 lineas

--- src/it/polito/teaching/cv/KalmanFiltering.java
+++ src/it/polito/teaching/cv/KalmanFiltering.java
@@ -1,8 +1,11 @@
 package it.polito.teaching.cv;
 
-import org.opencv.core.CvType;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.core.*;
 import org.opencv.video.KalmanFilter;
 import org.opencv.videoio.*;
@@ -14,6 +17,9 @@ public class KalmanFiltering {
 	private static int contrSize = 0;
 	
 	private static final int type = CvType.CV_32F;
+	
+	private static final int MIN_H_BLUE = 200;
+	private static final int MAX_H_BLUE = 300;
 	
 	public static void main(String[] args){
 		
@@ -93,6 +99,7 @@ public class KalmanFiltering {
 					System.out.println("State post: ");
 					System.out.println(state.dump());
 					
+					//INIT Rectangle of prediction and circle
 					Rect predRect = new Rect();
 					
 					predRect.width = (int) state.get(0, 4)[0];
@@ -105,13 +112,57 @@ public class KalmanFiltering {
 					center.y = (int) state.get(0, 1)[0];
 					
 					Imgproc.circle(frame, center, 2, new Scalar(255,0,0), -1);
-					//Imgproc.rectangle(frame, predRect, new Scalar(255,0,0), 2);
+					Imgproc.rectangle(frame, new Point(predRect.x,predRect.y), new Point(predRect.x+predRect.width,predRect.y+predRect.height), new Scalar(255,0,0),2);
 					
+					// Init feautiring operations
+					Mat blurredImage = new Mat();
+					Mat hsvImage = new Mat();
+					
+					Imgproc.GaussianBlur(frame, blurredImage, new Size(5,5),3.0,3.0);
+					
+					Imgproc.cvtColor(blurredImage, hsvImage, Imgproc.COLOR_BGR2HSV);
+					
+					Mat rangeRes = Mat.zeros(frame.size(), CvType.CV_8UC1);
+					
+					Core.inRange(blurredImage, new Scalar(MIN_H_BLUE/2,100,80), new Scalar(MAX_H_BLUE/2,255,255), rangeRes);
+					
+					Imgproc.erode(rangeRes, rangeRes, new Mat(), new Point(-1,-1),2);
+					Imgproc.dilate(rangeRes, rangeRes, new Mat(), new Point(-1,-1),2);
+					
+					List<MatOfPoint> contours = new ArrayList<>();
+					Mat hierarchy = new Mat();
+					
+					Imgproc.findContours(rangeRes, contours,hierarchy,Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);
+					
+					List<MatOfPoint> balls = new ArrayList<>();
+					List<Rect> ballsBox = new ArrayList<>();
+					
+					for (int i = 0; i<contours.size(); i++)
+					{
+						Rect bBox = new Rect();
+						bBox = Imgproc.boundingRect(contours.get(i));
+						float ratio = (float) bBox.width / (float) bBox.height;
+						if (ratio > 1.0f){
+							ratio = 1.0f /ratio;
+						}
+						
+				        if (ratio > 0.75 && bBox.area() >= 400)
+				         {
+				            balls.add(contours.get(i));
+				            ballsBox.add(bBox);
+				         }
+					}
+					
+					System.out.println("Balls found: " + String.valueOf(balls.size()));
+					
+					for (int i = 0; i < balls.size(); i++){
+						Imgproc.drawContours(frame, balls, i, new Scalar(20, 150, 20),1);
+						//Seguir linea 152
+					}
 				}
 			}
 			
 		}
-		//Seguir
 		
 		
 	}
